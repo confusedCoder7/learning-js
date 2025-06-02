950d9582786b484735cabe642ebeac1a25bb51e0
Akshay Kumar <klucifer911@gmail.com>
2025-06-02 17:32:12 +0530

    variables

---
+++ 01-basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "example@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 334223 
+accountEmail = "new@gmail.com"
+accountPassword = "232532"
+accountCity = "Pune"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
