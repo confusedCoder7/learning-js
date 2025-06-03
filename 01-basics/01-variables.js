f0621df4e04b37e4d155ec57c170f09167f6600a
Akshay Kumar <klucifer911@gmail.com>
2025-06-03 08:27:17 +0530

    Learning Continued

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "example@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 334223 
+accountEmail = "new@gmail.com"
+accountPassword = "232532"
+accountCity = "Pune"
+var accountPassword = "99999"
+{
+    var accountPassword = "avababgg"
+}
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
