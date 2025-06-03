3b258a087e4cbd85e0bbbd73a8753c80ddbe50cd
Akshay Kumar <klucifer911@gmail.com>
2025-06-03 09:04:15 +0530

    Learning continued

---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // treats all js code as ModernJS code 
+
+// alert(5 + 2)  Nodejs Environment
+
+console.log(    3 
+        +
+                        3
+                                );
+    console.log(         "Hello"                
+            
+    )           // NO Code Readability (Bad Practice)
+
+let age = 18
+let name = "Sam"
+let isLoggedIn = false
+let undefinedValue = undefined
+let nullValue = null
+let bigintValue = 2357901250915125n
+
+console.log(typeof "string")
+console.log(typeof(isLoggedIn))
+console.log(typeof null)
+console.log(typeof undefined)
